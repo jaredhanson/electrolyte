79bd5feffef983cbb246d1177e5ecd872bb54de1
Jared Hanson <jaredhanson@gmail.com>
2013-11-16 13:28:38 -0800

    Throw error from Component#instantiate.

---
+++ test/component.test.js
@@ -0,0 +1,22 @@
+/* global describe, it, expect */
+
+var Component = require('../lib/component');
+
+
+describe('Component', function() {
+  
+  describe('#create', function() {
+    
+    describe('abstract component', function() {
+      var comp = new Component('foo', []);
+      
+      it('should throw an error', function() {
+        expect(function() {
+          comp.create();
+        }).to.throw(Error, "Unable to instantiate component 'foo'");
+      });
+    });
+    
+  });
+  
+});
