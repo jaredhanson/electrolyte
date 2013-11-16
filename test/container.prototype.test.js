415d0ec99149421baba886b341638a49ab7980d2
Jared Hanson <jaredhanson@gmail.com>
2013-11-16 11:27:27 -0800

    Additional test coverage for Container#prototype.

--- test/container.prototype.test.js
+++ test/container.prototype.test.js
@@ -15,11 +15,12 @@ Bacteria.prototype.eat = function() {
 
 
 describe('Container#prototype', function() {
-  var container = new Container();
-  var proto = new Bacteria('starch');
-  container.prototype('bacteria', proto);
   
   describe('creating an object', function() {
+    var container = new Container();
+    var proto = new Bacteria('starch');
+    container.prototype('bacteria', proto);
+    
     var obj = container.create('bacteria');
     
     it('should create an object', function() {
@@ -37,4 +38,28 @@ describe('Container#prototype', function() {
       expect(obj).to.not.be.equal(obj2);
     });
   });
+  
+  describe('creating an object from object literal', function() {
+    var container = new Container();
+    var proto = { foo: 'bar' };
+    container.prototype('foo', proto);
+    
+    var obj = container.create('foo');
+    
+    it('should create an object', function() {
+      expect(obj).to.be.an('object');
+      expect(obj).to.be.an.instanceOf(Object);
+      expect(obj.__proto__).to.equal(proto);
+    });
+    
+    it('should create object with prototype properties', function() {
+      expect(obj.foo).to.equal('bar');
+    });
+    
+    it('should create unique instances', function() {
+      var obj2 = container.create('foo');
+      expect(obj).to.not.be.equal(obj2);
+    });
+  });
+  
 });
