4c02848eeb0515af7ebba6e6211b7f1820ec6ae3
Jared Hanson <jaredhanson@gmail.com>
2013-11-24 17:36:30 -0800

    Restructure example app.

---
+++ examples/express/README.md
@@ -0,0 +1,2 @@
+# Express App using Electrolyte
+
--- examples/express/etc/init/00_ioc.js
+++ examples/express/etc/init/00_ioc.js
@@ -2,5 +2,5 @@ var ioc = require('electrolyte');
 
 module.exports = function() {
   ioc.loader('handlers', ioc.node('app/handlers'));
-  ioc.loader(ioc.node('app/objects'));
+  ioc.loader(ioc.node('app/components'));
 }
