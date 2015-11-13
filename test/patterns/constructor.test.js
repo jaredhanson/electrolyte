9044fb73f939357fa09e26569c332a31a420bdd6
bosgood <bosgood@gmail.com>
2015-11-13 16:57:38 -0500

    Fixing tests after breaking changes

--- test/patterns/constructor.test.js
+++ test/patterns/constructor.test.js
@@ -13,7 +13,7 @@ describe('Constructor', function() {
     it('should throw an error', function() {
       expect(function() {
         ctor.instantiate('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
-      }).to.throw(Error, "Constructor for component 'animal' requires too many arguments");
+      }).to.throw(Error, "Constructor for object 'animal' requires too many arguments");
     });
   });
 });
