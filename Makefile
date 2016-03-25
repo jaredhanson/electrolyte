49583f66e18b7ce4c2e5619addf3ca472e5de903
Jared Hanson <jaredhanson@gmail.com>
2016-03-24 17:54:44 -0700

    Rearrange tests, clarify comments.

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ include node_modules/make-node/main.mk
 
 
 SOURCES ?= lib/*.js
-TESTS ?= test/*.test.js test/**/*.test.js test/integration/node/*.test.js
+#TESTS ?= test/*.test.js test/**/*.test.js test/integration/node/*.test.js
+TESTS ?= test/*.test.js test/**/*.test.js
 
 LCOVFILE = ./reports/coverage/lcov.info
 
--- test/container.test.js
+++ test/container.test.js
@@ -4,37 +4,54 @@ var Container = require('../lib/container');
 
 
 describe('Container', function() {
-  var container = new Container();
   
   describe('#create', function() {
+    var container = new Container();
     
     describe('unknown component', function() {
-      it('should throw an error', function() {
+      it('should throw an error when created without a parent', function() {
         expect(function() {
           var obj = container.create('unknown');
         }).to.throw(Error, 'Unable to create object "unknown" required by: unknown');
       });
-    });
-
-    describe('traditional module as a component', function() {
-      var container = new Container();
-      container.use(require('../lib/loaders/node_modules')());
-
-      it('return the same as require', function() {
-        var obj = container.create('dgram');
-        expect(obj).to.be.equal(require('dgram'));
+      
+      it('should throw an error when created with a parent', function() {
+        expect(function() {
+          var obj = container.create('unknown', { id: 'main' });
+        }).to.throw(Error, 'Unable to create object "unknown" required by: main');
+      });
+      
+      it('should throw an error when created with a parent, lacking an id', function() {
+        expect(function() {
+          var obj = container.create('unknown', {});
+        }).to.throw(Error, 'Unable to create object "unknown" required by: unknown');
       });
     });
-
-    describe('unknown loader', function() {
+    
+  });
+  
+  describe('#use', function() {
+    var container = new Container();
+    
+    describe('invalid loader', function() {
       it('should throw an error', function() {
         expect(function() {
-          container.use('test',undefined);
+          container.use('test', undefined);
         }).to.throw(Error, "Container#use requires a function, was passed a 'undefined'");
       });
     });
-  
     
+  });
+  
+  
+  describe('using node_modules loader', function() {
+    var container = new Container();
+    container.use(require('../lib/loaders/node_modules')());
+
+    it('return the same module as require', function() {
+      var obj = container.create('dgram');
+      expect(obj).to.be.equal(require('dgram'));
+    });
   });
   
 });
