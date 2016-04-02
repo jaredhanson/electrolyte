4eee16725fef24165c907c53fbcaf718f857daaf
Jared Hanson <jaredhanson@gmail.com>
2016-04-02 11:50:49 -0700

    More test cases.

--- test/container.test.js
+++ test/container.test.js
@@ -74,9 +74,37 @@ describe('Container', function() {
     var container = new Container();
     container.use('cache', memcached);
     
-    it('should create logger', function() {
+    it('should create Memcached implementation of cache', function() {
       var obj = container.create('cache/cache');
       expect(obj).to.be.an.instanceof(memcached.MemcachedCache);
+    });
+  });
+  
+  describe('using Redis cache source in two namespaces', function() {
+    var redis = require('./fixtures/sources/cache-redis');
+    
+    var container = new Container();
+    container.use('cache', redis);
+    container.use('cache/pages', redis);
+    
+    var cache1 = container.create('cache/cache');
+    var cache2 = container.create('cache/cache');
+    
+    var pagecache1 = container.create('cache/pages/cache');
+    var pagecache2 = container.create('cache/pages/cache');
+    
+    it('should create singleton instance of Redis cache in first namespace', function() {
+      expect(cache1).to.be.an.instanceof(redis.RedisCache);
+      expect(cache1).to.be.equal(cache2);
+    });
+    
+    it('should create singleton instance of Redis cache in second namespace', function() {
+      expect(pagecache1).to.be.an.instanceof(redis.RedisCache);
+      expect(pagecache1).to.be.equal(pagecache2);
+    });
+    
+    it('should create separate instances of Redis cache in different namespaces', function() {
+      expect(cache1).to.not.be.equal(pagecache1);
     });
   });
   
@@ -88,7 +116,7 @@ describe('Container', function() {
     container.use('cache', memcached);
     container.use('cache', redis);
     
-    it('should create logger', function() {
+    it('should create Redis implementation of cache', function() {
       var obj = container.create('cache/cache');
       expect(obj).to.be.an.instanceof(redis.RedisCache);
     });
