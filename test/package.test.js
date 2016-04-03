bf1f843f40e8d7f5ce025d12d174125191c9d8fb
Jared Hanson <jaredhanson@gmail.com>
2016-04-03 10:20:48 -0700

    Add test cases for exports.

--- test/package.test.js
+++ test/package.test.js
@@ -8,4 +8,17 @@ describe('electrolyte', function() {
     expect(electrolyte.Container).to.be.a('function');
   });
   
+  it('should export sources', function() {
+    expect(electrolyte.dir).to.be.a('function');
+    expect(electrolyte.node_modules).to.be.a('function');
+  });
+  
+  it('should alias fs source to dir', function() {
+    expect(electrolyte.fs).to.be.equal(electrolyte.dir);
+  });
+  
+  it('should export deprecated sources', function() {
+    expect(electrolyte.node).to.be.a('function');
+  });
+  
 });
